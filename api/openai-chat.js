c9666565f4235457e85e427664b61830d7cbf2fa
Mrbantiyt <kkrishna87937@gmail.com>
2026-07-09 19:04:02 +0530

    Update openai-chat.js

--- api/openai-chat.js
+++ api/openai-chat.js
@@ -88,7 +88,7 @@ export default async function handler(req, res) {
           },
           ...openaiMessages,
         ],
-        max_tokens: 800000,
+        max_tokens: 15740,
       }),
     });
 
